e9269affb48997c971b541a1f04b575841b2eaa1
Himanshu Gupta <himjan1416@gmail.com>
2017-06-30 00:20:54 +0530

    renamed the main directory for ease in terminal

--- Python for everybody Michigan/2 Python data stuctures/6.5.py
+++
@@ -1,5 +0,0 @@
-text = "X-DSPAM-Confidence:    0.8475"
-index=text.find('0')
-abc = text[index: ]
-x=float(abc)
-print x
--- Python for everybody Michigan/2 Python data stuctures/7.1.py
+++
@@ -1,7 +0,0 @@
-# Use words.txt as the file name
-fname = raw_input("Enter file name: ")
-fh = open(fname)
-for x in fh:
-    x=x.rstrip()
-    print x.upper()
-
